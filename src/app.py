fbb4c66c842760604fe5e6c6393e937c4127208f
Alfandavi Aryo Utomo <alfanau18@gmail.com>
2021-09-27 22:59:26 +0700

    sek kesel

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import Todo
 app = Flask(__name__)
-                           
+app.config['SECRET_KEY'] = 'password'
+
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     request_method = request.method
@@ -9,11 +12,17 @@ def hello_world():
         # last_name = request.form['last_name']
         return redirect(url_for('name',first_name=first_name))
 
-    return render_template('hello.html',request_method=request_method)
+    return render_template('index.html',request_method=request_method)
 
 @app.route('/name/<string:first_name>')
 def name(first_name):
     return f'{first_name}'
+
+@app.route('/todo', methods=['GET','POST'])
+def todo():
+    todo_form = Todo()
+    return render_template('todo.html', form=todo_form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
