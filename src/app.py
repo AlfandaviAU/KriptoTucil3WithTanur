9eae732517e2d88d2238633c0372e8fb4b87eb72
Alfandavi Aryo Utomo <alfanau18@gmail.com>
2021-09-27 14:55:31 +0700

    bengi neh
    bengi neh

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
                            
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return "<h1>Hello World!</h1>"
+    request_method = request.method
+    if request_method == 'POST':
+        first_name = request.form['first_name']
+        # last_name = request.form['last_name']
+        return redirect(url_for('name',first_name=first_name))
+
+    return render_template('hello.html',request_method=request_method)
+
+@app.route('/name/<string:first_name>')
+def name(first_name):
+    return f'{first_name}'
 
 if __name__ == '__main__':
     app.run(debug=True)
