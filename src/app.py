b16c742648fd6ff93c106f92a720f2b4e050cf59
Alfandavi Aryo Utomo <alfanau18@gmail.com>
2021-09-28 23:34:21 +0700

    asu

--- src/app.py
+++ src/app.py
@@ -5,23 +5,30 @@ app.config['SECRET_KEY'] = 'password'
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def main():
     request_method = request.method
-    if request_method == 'POST':
-        first_name = request.form['first_name']
-        # last_name = request.form['last_name']
-        return redirect(url_for('name',first_name=first_name))
-
     return render_template('index.html',request_method=request_method)
 
-@app.route('/name/<string:first_name>')
-def name(first_name):
-    return f'{first_name}'
 
-@app.route('/todo', methods=['GET','POST'])
-def todo():
-    todo_form = Todo()
-    return render_template('todo.html', form=todo_form)
+@app.route('/rc4', methods=['GET', 'POST'])
+def page_rc4():
+    request_method = request.method
+    return render_template('rc4.html',request_method=request_method)
+
+
+@app.route('/stegano', methods=['GET', 'POST'])
+def page_stegano():
+    request_method = request.method
+    return render_template('stegano.html',request_method=request_method)
+
+# @app.route('/name/<string:first_name>')
+# def name(first_name):
+#     return f'{first_name}'
+
+# @app.route('/todo', methods=['GET','POST'])
+# def todo():
+#     todo_form = Todo()
+#     return render_template('todo.html', form=todo_form)
 
 
 if __name__ == '__main__':
